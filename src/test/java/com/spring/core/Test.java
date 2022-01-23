8bb680b8329969c2213f83f0a6fa08208af127fe
manju07 <manjunathasundi07@gmail.com>
2022-01-23 12:26:03 +0530

    test

--- src/test/java/com/spring/core/Test.java
+++ src/test/java/com/spring/core/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public void test() {
         System.out.println("hey trying stash");       
         System.out.println("stash try");
+        System.out.println("trying tag");
     }
 }
