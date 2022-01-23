f58ba0f483aa0e1d65c1d536ec83d808dfd220dc
manju07 <manjunathasundi07@gmail.com>
2022-01-23 12:24:01 +0530

    stash trying

--- src/test/java/com/spring/core/Test.java
+++ src/test/java/com/spring/core/Test.java
@@ -2,6 +2,7 @@ package com.spring.core;
 
 public class Test {
     public void test() {
-        
+        System.out.println("hey trying stash");       
+        System.out.println("stash try");
     }
 }
