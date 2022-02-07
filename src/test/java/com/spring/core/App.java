0aef22e976732f1ed4d412013fd175af975f59ee
manju07 <manjunathasundi07@gmail.com>
2022-02-07 15:18:32 +0530

    Revert "hello test"
    This reverts commit 30c2c774625bbfb2068081a6edeea91855364d59.

--- src/test/java/com/spring/core/App.java
+++ src/test/java/com/spring/core/App.java
@@ -100,7 +100,6 @@ public class App {
         long endTime = System.currentTimeMillis();
         System.out.println("Over all time taken = " + ((endTime - startTime) / 1000) + " seconds");
         System.out.println("hello");
-        System.out.println("hello 123");
         System.out.println();
     }
 
