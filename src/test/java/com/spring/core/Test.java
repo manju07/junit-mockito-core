06fa75961075efa3513175e1bbd3095143004b04
manju07 <manjunathasundi07@gmail.com>
2022-02-08 15:45:35 +0530

    test from master

--- src/test/java/com/spring/core/Test.java
+++ src/test/java/com/spring/core/Test.java
@@ -4,8 +4,11 @@ public class Test {
     public void test() {
         System.out.println("hey trying stash"); 
               
-        System.out.println("stash try");
 
-        System.out.println("modifying content");
+        System.out.println("stash try changed");
+
+
+
+        System.out.println("modifying content changed");
     }
 }
