4b3392f5b842b21aeb97362416fb8b0e1aee0be1
manju07 <manjunathasundi07@gmail.com>
2022-02-08 15:32:55 +0530

    sample modification

--- src/test/java/com/spring/core/Test.java
+++ src/test/java/com/spring/core/Test.java
@@ -2,8 +2,10 @@ package com.spring.core;
 
 public class Test {
     public void test() {
-        System.out.println("hey trying stash");       
+        System.out.println("hey trying stash"); 
+              
         System.out.println("stash try");
+
         System.out.println("modifying content");
     }
 }
