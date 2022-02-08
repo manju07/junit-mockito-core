edb61f3419f55bfde6d48949a584834f092af82f
manju07 <manjunathasundi07@gmail.com>
2022-02-08 15:44:57 +0530

    test

--- src/test/java/com/spring/core/Test.java
+++ src/test/java/com/spring/core/Test.java
@@ -3,9 +3,7 @@ package com.spring.core;
 public class Test {
     public void test() {
         System.out.println("hey trying stash"); 
-              
         System.out.println("stash try");
-
         System.out.println("modifying content");
     }
 }
